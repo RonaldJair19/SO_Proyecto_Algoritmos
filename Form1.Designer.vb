9434fcc46bd241fbcdf9adcfd130566d0ec4173a
Ronald Juarez <jairudas1998@gmail.com>
2020-06-30 21:49:14 -0500

    Funciona la entrada de datos del primera algoritmo

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -24,22 +24,22 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Me.TabControl1 = New System.Windows.Forms.TabControl()
         Me.TabPageEnvejecimiento = New System.Windows.Forms.TabPage()
+        Me.ButtonAgregar = New System.Windows.Forms.Button()
+        Me.ButtonTerminar = New System.Windows.Forms.Button()
+        Me.TextBoxValorPulsos = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBoxPulsos = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.N_ContBits = New System.Windows.Forms.NumericUpDown()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.TB_CuadrosPag = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
         Me.TabPageSC = New System.Windows.Forms.TabPage()
         Me.TabPageSegmentacion = New System.Windows.Forms.TabPage()
         Me.TabPageP_M_PA = New System.Windows.Forms.TabPage()
         Me.TabPageSP = New System.Windows.Forms.TabPage()
         Me.Panel1 = New System.Windows.Forms.Panel()
-        Me.Label1 = New System.Windows.Forms.Label()
-        Me.Label2 = New System.Windows.Forms.Label()
-        Me.TB_CuadrosPag = New System.Windows.Forms.TextBox()
-        Me.Label3 = New System.Windows.Forms.Label()
-        Me.N_ContBits = New System.Windows.Forms.NumericUpDown()
-        Me.Label4 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.Button2 = New System.Windows.Forms.Button()
-        Me.Button3 = New System.Windows.Forms.Button()
         Me.TabControl1.SuspendLayout()
         Me.TabPageEnvejecimiento.SuspendLayout()
         CType(Me.N_ContBits, System.ComponentModel.ISupportInitialize).BeginInit()
@@ -63,11 +63,11 @@ Partial Class Form1
         '
         Me.TabPageEnvejecimiento.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
         Me.TabPageEnvejecimiento.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.TabPageEnvejecimiento.Controls.Add(Me.Button3)
-        Me.TabPageEnvejecimiento.Controls.Add(Me.Button2)
-        Me.TabPageEnvejecimiento.Controls.Add(Me.TextBox2)
+        Me.TabPageEnvejecimiento.Controls.Add(Me.ButtonAgregar)
+        Me.TabPageEnvejecimiento.Controls.Add(Me.ButtonTerminar)
+        Me.TabPageEnvejecimiento.Controls.Add(Me.TextBoxValorPulsos)
         Me.TabPageEnvejecimiento.Controls.Add(Me.Button1)
-        Me.TabPageEnvejecimiento.Controls.Add(Me.TextBox1)
+        Me.TabPageEnvejecimiento.Controls.Add(Me.TextBoxPulsos)
         Me.TabPageEnvejecimiento.Controls.Add(Me.Label4)
         Me.TabPageEnvejecimiento.Controls.Add(Me.N_ContBits)
         Me.TabPageEnvejecimiento.Controls.Add(Me.Label3)
@@ -82,113 +82,51 @@ Partial Class Form1
         Me.TabPageEnvejecimiento.Text = "A. de Envejecimiento"
         Me.TabPageEnvejecimiento.UseVisualStyleBackColor = True
         '
-        'TabPageSC
+        'ButtonAgregar
         '
-        Me.TabPageSC.Location = New System.Drawing.Point(4, 27)
-        Me.TabPageSC.Name = "TabPageSC"
-        Me.TabPageSC.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPageSC.Size = New System.Drawing.Size(898, 534)
-        Me.TabPageSC.TabIndex = 1
-        Me.TabPageSC.Text = "A. Sistema Compañero"
-        Me.TabPageSC.UseVisualStyleBackColor = True
+        Me.ButtonAgregar.BackColor = System.Drawing.Color.DarkRed
+        Me.ButtonAgregar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.ButtonAgregar.Cursor = System.Windows.Forms.Cursors.Default
+        Me.ButtonAgregar.Enabled = False
+        Me.ButtonAgregar.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.ButtonAgregar.FlatAppearance.BorderSize = 2
+        Me.ButtonAgregar.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.ButtonAgregar.Font = New System.Drawing.Font("Corbel", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonAgregar.ForeColor = System.Drawing.Color.White
+        Me.ButtonAgregar.Location = New System.Drawing.Point(546, 97)
+        Me.ButtonAgregar.Name = "ButtonAgregar"
+        Me.ButtonAgregar.Size = New System.Drawing.Size(90, 35)
+        Me.ButtonAgregar.TabIndex = 12
+        Me.ButtonAgregar.Text = "Agregar"
+        Me.ButtonAgregar.UseVisualStyleBackColor = False
         '
-        'TabPageSegmentacion
+        'ButtonTerminar
         '
-        Me.TabPageSegmentacion.Location = New System.Drawing.Point(4, 27)
-        Me.TabPageSegmentacion.Name = "TabPageSegmentacion"
-        Me.TabPageSegmentacion.Size = New System.Drawing.Size(898, 534)
-        Me.TabPageSegmentacion.TabIndex = 2
-        Me.TabPageSegmentacion.Text = "Segmentación"
-        Me.TabPageSegmentacion.UseVisualStyleBackColor = True
+        Me.ButtonTerminar.BackColor = System.Drawing.Color.DarkRed
+        Me.ButtonTerminar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.ButtonTerminar.Cursor = System.Windows.Forms.Cursors.Default
+        Me.ButtonTerminar.Enabled = False
+        Me.ButtonTerminar.FlatAppearance.BorderColor = System.Drawing.Color.Black
+        Me.ButtonTerminar.FlatAppearance.BorderSize = 2
+        Me.ButtonTerminar.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.ButtonTerminar.Font = New System.Drawing.Font("Corbel", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ButtonTerminar.ForeColor = System.Drawing.Color.White
+        Me.ButtonTerminar.Location = New System.Drawing.Point(752, 97)
+        Me.ButtonTerminar.Name = "ButtonTerminar"
+        Me.ButtonTerminar.Size = New System.Drawing.Size(102, 35)
+        Me.ButtonTerminar.TabIndex = 11
+        Me.ButtonTerminar.Text = "Terminar"
+        Me.ButtonTerminar.UseVisualStyleBackColor = False
         '
-        'TabPageP_M_PA
+        'TextBoxValorPulsos
         '
-        Me.TabPageP_M_PA.Location = New System.Drawing.Point(4, 27)
-        Me.TabPageP_M_PA.Name = "TabPageP_M_PA"
-        Me.TabPageP_M_PA.Size = New System.Drawing.Size(898, 534)
-        Me.TabPageP_M_PA.TabIndex = 3
-        Me.TabPageP_M_PA.Text = "A. P_M_PA"
-        Me.TabPageP_M_PA.UseVisualStyleBackColor = True
-        '
-        'TabPageSP
-        '
-        Me.TabPageSP.Location = New System.Drawing.Point(4, 27)
-        Me.TabPageSP.Name = "TabPageSP"
-        Me.TabPageSP.Size = New System.Drawing.Size(898, 534)
-        Me.TabPageSP.TabIndex = 4
-        Me.TabPageSP.Text = "A. Sustitución de Páginas"
-        Me.TabPageSP.UseVisualStyleBackColor = True
-        '
-        'Panel1
-        '
-        Me.Panel1.BackColor = System.Drawing.Color.Maroon
-        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
-        Me.Panel1.Location = New System.Drawing.Point(2, 1)
-        Me.Panel1.Name = "Panel1"
-        Me.Panel1.RightToLeft = System.Windows.Forms.RightToLeft.No
-        Me.Panel1.Size = New System.Drawing.Size(926, 141)
-        Me.Panel1.TabIndex = 1
-        '
-        'Label1
-        '
-        Me.Label1.AutoSize = True
-        Me.Label1.Font = New System.Drawing.Font("Verdana", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label1.Location = New System.Drawing.Point(279, 14)
-        Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(315, 25)
-        Me.Label1.TabIndex = 0
-        Me.Label1.Text = "Algoritmo de envejecimiento"
-        '
-        'Label2
-        '
-        Me.Label2.AutoSize = True
-        Me.Label2.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label2.Location = New System.Drawing.Point(17, 64)
-        Me.Label2.Name = "Label2"
-        Me.Label2.Size = New System.Drawing.Size(356, 18)
-        Me.Label2.TabIndex = 1
-        Me.Label2.Text = "Ingrese la cantidad de cuadros de página:"
-        '
-        'TB_CuadrosPag
-        '
-        Me.TB_CuadrosPag.Location = New System.Drawing.Point(379, 61)
-        Me.TB_CuadrosPag.Name = "TB_CuadrosPag"
-        Me.TB_CuadrosPag.Size = New System.Drawing.Size(100, 27)
-        Me.TB_CuadrosPag.TabIndex = 2
-        '
-        'Label3
-        '
-        Me.Label3.AutoSize = True
-        Me.Label3.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label3.Location = New System.Drawing.Point(17, 106)
-        Me.Label3.Name = "Label3"
-        Me.Label3.Size = New System.Drawing.Size(337, 18)
-        Me.Label3.TabIndex = 3
-        Me.Label3.Text = "Ingrese el tamaño del contador en bits:" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
-        '
-        'N_ContBits
-        '
-        Me.N_ContBits.Location = New System.Drawing.Point(379, 104)
-        Me.N_ContBits.Name = "N_ContBits"
-        Me.N_ContBits.Size = New System.Drawing.Size(100, 27)
-        Me.N_ContBits.TabIndex = 5
-        '
-        'Label4
-        '
-        Me.Label4.AutoSize = True
-        Me.Label4.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(499, 64)
-        Me.Label4.Name = "Label4"
-        Me.Label4.Size = New System.Drawing.Size(230, 18)
-        Me.Label4.TabIndex = 6
-        Me.Label4.Text = "Agrege los pulsos de reloj:"
-        '
-        'TextBox1
-        '
-        Me.TextBox1.Location = New System.Drawing.Point(742, 61)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(146, 27)
-        Me.TextBox1.TabIndex = 7
+        Me.TextBoxValorPulsos.Location = New System.Drawing.Point(502, 138)
+        Me.TextBoxValorPulsos.Multiline = True
+        Me.TextBoxValorPulsos.Name = "TextBoxValorPulsos"
+        Me.TextBoxValorPulsos.ReadOnly = True
+        Me.TextBoxValorPulsos.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal
+        Me.TextBoxValorPulsos.Size = New System.Drawing.Size(386, 88)
+        Me.TextBoxValorPulsos.TabIndex = 10
         '
         'Button1
         '
@@ -207,48 +145,114 @@ Partial Class Form1
         Me.Button1.Text = "Aceptar"
         Me.Button1.UseVisualStyleBackColor = False
         '
-        'TextBox2
+        'TextBoxPulsos
         '
-        Me.TextBox2.Location = New System.Drawing.Point(502, 138)
-        Me.TextBox2.Multiline = True
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.ReadOnly = True
-        Me.TextBox2.Size = New System.Drawing.Size(386, 88)
-        Me.TextBox2.TabIndex = 10
+        Me.TextBoxPulsos.Location = New System.Drawing.Point(742, 61)
+        Me.TextBoxPulsos.Name = "TextBoxPulsos"
+        Me.TextBoxPulsos.Size = New System.Drawing.Size(146, 27)
+        Me.TextBoxPulsos.TabIndex = 7
         '
-        'Button2
+        'Label4
         '
-        Me.Button2.BackColor = System.Drawing.Color.DarkRed
-        Me.Button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
-        Me.Button2.Cursor = System.Windows.Forms.Cursors.Default
-        Me.Button2.FlatAppearance.BorderColor = System.Drawing.Color.Black
-        Me.Button2.FlatAppearance.BorderSize = 2
-        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.Button2.Font = New System.Drawing.Font("Corbel", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button2.ForeColor = System.Drawing.Color.White
-        Me.Button2.Location = New System.Drawing.Point(752, 97)
-        Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(123, 35)
-        Me.Button2.TabIndex = 11
-        Me.Button2.Text = "Terminar"
-        Me.Button2.UseVisualStyleBackColor = False
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(499, 64)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(230, 18)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Agrege los pulsos de reloj:"
         '
-        'Button3
+        'N_ContBits
         '
-        Me.Button3.BackColor = System.Drawing.Color.DarkRed
-        Me.Button3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
-        Me.Button3.Cursor = System.Windows.Forms.Cursors.Default
-        Me.Button3.FlatAppearance.BorderColor = System.Drawing.Color.Black
-        Me.Button3.FlatAppearance.BorderSize = 2
-        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.Button3.Font = New System.Drawing.Font("Corbel", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Button3.ForeColor = System.Drawing.Color.White
-        Me.Button3.Location = New System.Drawing.Point(546, 97)
-        Me.Button3.Name = "Button3"
-        Me.Button3.Size = New System.Drawing.Size(90, 35)
-        Me.Button3.TabIndex = 12
-        Me.Button3.Text = "Agregar"
-        Me.Button3.UseVisualStyleBackColor = False
+        Me.N_ContBits.Location = New System.Drawing.Point(379, 104)
+        Me.N_ContBits.Name = "N_ContBits"
+        Me.N_ContBits.Size = New System.Drawing.Size(100, 27)
+        Me.N_ContBits.TabIndex = 5
+        Me.N_ContBits.Value = New Decimal(New Integer() {2, 0, 0, 0})
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(17, 106)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(337, 18)
+        Me.Label3.TabIndex = 3
+        Me.Label3.Text = "Ingrese el tamaño del contador en bits:" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+        '
+        'TB_CuadrosPag
+        '
+        Me.TB_CuadrosPag.Location = New System.Drawing.Point(379, 61)
+        Me.TB_CuadrosPag.Name = "TB_CuadrosPag"
+        Me.TB_CuadrosPag.Size = New System.Drawing.Size(100, 27)
+        Me.TB_CuadrosPag.TabIndex = 2
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(17, 64)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(356, 18)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Ingrese la cantidad de cuadros de página:"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Verdana", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(279, 14)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(315, 25)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Algoritmo de envejecimiento"
+        '
+        'TabPageSC
+        '
+        Me.TabPageSC.Location = New System.Drawing.Point(4, 27)
+        Me.TabPageSC.Name = "TabPageSC"
+        Me.TabPageSC.Padding = New System.Windows.Forms.Padding(3)
+        Me.TabPageSC.Size = New System.Drawing.Size(898, 555)
+        Me.TabPageSC.TabIndex = 1
+        Me.TabPageSC.Text = "A. Sistema Compañero"
+        Me.TabPageSC.UseVisualStyleBackColor = True
+        '
+        'TabPageSegmentacion
+        '
+        Me.TabPageSegmentacion.Location = New System.Drawing.Point(4, 27)
+        Me.TabPageSegmentacion.Name = "TabPageSegmentacion"
+        Me.TabPageSegmentacion.Size = New System.Drawing.Size(898, 555)
+        Me.TabPageSegmentacion.TabIndex = 2
+        Me.TabPageSegmentacion.Text = "Segmentación"
+        Me.TabPageSegmentacion.UseVisualStyleBackColor = True
+        '
+        'TabPageP_M_PA
+        '
+        Me.TabPageP_M_PA.Location = New System.Drawing.Point(4, 27)
+        Me.TabPageP_M_PA.Name = "TabPageP_M_PA"
+        Me.TabPageP_M_PA.Size = New System.Drawing.Size(898, 555)
+        Me.TabPageP_M_PA.TabIndex = 3
+        Me.TabPageP_M_PA.Text = "A. P_M_PA"
+        Me.TabPageP_M_PA.UseVisualStyleBackColor = True
+        '
+        'TabPageSP
+        '
+        Me.TabPageSP.Location = New System.Drawing.Point(4, 27)
+        Me.TabPageSP.Name = "TabPageSP"
+        Me.TabPageSP.Size = New System.Drawing.Size(898, 555)
+        Me.TabPageSP.TabIndex = 4
+        Me.TabPageSP.Text = "A. Sustitución de Páginas"
+        Me.TabPageSP.UseVisualStyleBackColor = True
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Maroon
+        Me.Panel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Panel1.Location = New System.Drawing.Point(2, 1)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.Panel1.Size = New System.Drawing.Size(926, 141)
+        Me.Panel1.TabIndex = 1
         '
         'Form1
         '
@@ -280,9 +284,9 @@ Partial Class Form1
     Friend WithEvents Label3 As Label
     Friend WithEvents N_ContBits As NumericUpDown
     Friend WithEvents Button1 As Button
-    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBoxPulsos As TextBox
     Friend WithEvents Label4 As Label
-    Friend WithEvents TextBox2 As TextBox
-    Friend WithEvents Button3 As Button
-    Friend WithEvents Button2 As Button
+    Friend WithEvents TextBoxValorPulsos As TextBox
+    Friend WithEvents ButtonAgregar As Button
+    Friend WithEvents ButtonTerminar As Button
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,22 +1,48 @@
 ﻿Public Class Form1
+    Public Vector(15) As Integer
+    Public Cont As Integer = 0
     Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
 
     End Sub
 
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles ButtonTerminar.Click
         Dim dgv As New DataGridView
         Controls.Add(dgv)
-        dgv.Location = New Point(12, 12)
-        dgv.Size = New Size(250, 250)
-        Dim c As Integer = CInt(InputBox("How many columns", "Columns", "3"))
-        Dim r As Integer = CInt(InputBox("How many rows", "Rows", "3"))
+        dgv.Location = New Point(12, 260)
+        dgv.Size = New Size(870, 250)
+        'Dim c As Integer = CInt(InputBox("How many columns", "Columns", "3"))
+        Dim c As Integer = Val(N_ContBits.Text)
+        Dim r As Integer = Val(TB_CuadrosPag.Text)
+        Dim i As Integer
 
-        For cc As Integer = 0 To c - 1
+        For cc As Integer = 0 To c
             Dim nc As New DataGridViewTextBoxColumn
-            nc.Name = "Column" & cc.ToString
+            If cc = 0 Then
+                nc.Name = "Bits"
+            ElseIf cc = c Then
+                nc.Name = "Suma"
+            Else
+                nc.Name = Vector(cc - 1).ToString
+            End If
             dgv.Columns.Add(nc)
         Next
         dgv.Rows.Add(r - 1)
         TabControl1.TabPages(0).Controls.Add(dgv)
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ButtonAgregar.Enabled = True
+        ButtonTerminar.Enabled = True
+
+
+    End Sub
+
+    Private Sub ButtonAgregar_Click(sender As Object, e As EventArgs) Handles ButtonAgregar.Click
+        Dim Valor As Integer
+        Valor = Val(TextBoxPulsos.Text)
+        TextBoxValorPulsos.Text = TextBoxPulsos.Text + vbCrLf + TextBoxValorPulsos.Text
+        Vector(Cont) = Valor
+        Cont += 1
+        TextBoxPulsos.Clear()
     End Sub
 End Class
